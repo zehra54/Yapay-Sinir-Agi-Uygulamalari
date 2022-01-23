1936ad9f70529a001b2782e2acc83df632d8179f
zehra54 <96942021+zehra54@users.noreply.github.com>
2022-01-23 19:12:11 +0300

    Add files via upload

---
+++ 210231001.py
@@ -0,0 +1,27 @@
+giriş = "kalbin hızlı çalışması"
+print ("artışı_girin :\n")
+artışı_girin:
+
+girin_artısı = input ()
+0.45
+if float(artış_değeri) >= 0.70:
+    giriş = giriş + "koşu"
+elif float(artış_değeri) >= 0.60:
+    giriş = giriş + "yüzme"
+elif float(artış_değeri) >= 0.50:
+    giriş = giriş + "bisklet"
+elif float(artış_değeri) >= 0.40:
+    giriş = giriş + "voleybol"
+elif float(artış_değeri) >= 0.30:
+    giriş = giriş + "basketbol"
+elif float(artış_değeri) >= 0.20:
+    giriş = giriş + "futbol"
+elif float(artış_değeri) >= 0.10:
+    giriş = giriş + "yürüme"
+else:
+    giriş = giriş + "golf"
+    
+
+    
+
+
