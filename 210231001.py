c8b601c8fdc2af56c04865f0e213d4f37ab9e036
zehra54 <96942021+zehra54@users.noreply.github.com>
2022-01-24 07:07:56 +0300

    Update 210231001.py

--- 210231001.py
+++ 210231001.py
@@ -12,19 +12,19 @@ artışı_girin:
 
 girin_artısı = input ()
 0.45
-if float(artış_değeri) >= 0.70:
+if float(girin_artısı) >= 0.70:
     giriş = giriş + "koşu"
-elif float(artış_değeri) >= 0.60:
+elif float(girin_artısı) >= 0.60:
     giriş = giriş + "yüzme"
-elif float(artış_değeri) >= 0.50:
+elif float(girin_artısı) >= 0.50:
     giriş = giriş + "bisklet"
-elif float(artış_değeri) >= 0.40:
+elif float(girin_artısı) >= 0.40:
     giriş = giriş + "voleybol"
-elif float(artış_değeri) >= 0.30:
+elif float(girin_artısı) >= 0.30:
     giriş = giriş + "basketbol"
-elif float(artış_değeri) >= 0.20:
+elif float(girin_artısı) >= 0.20:
     giriş = giriş + "futbol"
-elif float(artış_değeri) >= 0.10:
+elif float(girin_artısı) >= 0.10:
     giriş = giriş + "yürüme"
 else:
     giriş = giriş + "golf"
